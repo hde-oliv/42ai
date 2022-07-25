f200c84931f5a84d9e30cd5c56578d061db08196
hde-oliv <henriquerocha@tutanota.com>
2022-07-25 19:04:56 -0300

    refactor: complete loading

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -1,13 +1,25 @@
+from datetime import date, datetime
 from sys import stdout
 from time import sleep
 
 
 def ft_progress(a_range: range):
-
+    total = len(a_range)
+    step = total / 20
+    start_time = datetime.now()
+    second_delta = datetime.now() - start_time
+    
     for i in a_range:
+        delta = datetime.now() - start_time
+        bar = "".join(["#"] * (int(i / step) % 20))
+
         stdout.write("\r")
-        stdout.write("[" + "".join(["#"] * (i % 10)))
+        stdout.write("ETA: {0:.1f} secs ".format(second_delta.seconds * (total - i) / 10**7))
+        stdout.write("[{0:>3.0f}%]".format(i * 100 / total))
+        stdout.write("[{0:<19}] ".format(bar))
+        stdout.write(f"{i + 1}/{total} | elapsed time {delta.seconds} secs")
         yield i
+        second_delta = second_delta - delta
     
 
 def main():
